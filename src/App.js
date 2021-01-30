e092f10bf845c9259a34512d50e8d7d9a422dc9e
Seif-Miehiar <Seif.Miehiar@gmail.com>
2021-01-30 05:30:34 +0200

    project component, cards init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import NavbarLandingPage from "./component/navbar";
 import SideNav from "./component/sideNav";
 
 function App() {
 	return (
 		<div>
-			{/* <NavbarLandingPage /> */}
-
 			<div className="App"></div>
 			<SideNav />
 		</div>
--- src/component/projects.js
+++ src/component/projects.js
@@ -1,29 +1,85 @@
-import Card from "react-bootstrap/Card";
-import { ListGroup, ListGroupItem } from "react-bootstrap/";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../style/projects.css";
+// import Card from "react-bootstrap/Card";
+// import { ListGroup, ListGroupItem } from "react-bootstrap/";
 
 function Projects() {
-	return (
+	const [projects, setProjects] = useState({ data: [] });
+
+	useEffect(async () => {
+		const result = await axios.get(
+			"http://localhost:3003/projectsApi/projects"
+		);
+
+		setProjects(result.data.data);
+	});
+	const iterate = (array) => {
+		var string = "";
+		array.forEach((element, i) => {
+			i < array.length - 1 ? (string += element + " - ") : (string += element);
+		});
+		return string;
+	};
+	return projects.length > 0 ? (
 		<div>
-			<Card style={{ width: "18rem" }}>
-				<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-				<Card.Body>
-					<Card.Title>Card Title</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</Card.Text>
-				</Card.Body>
-				<ListGroup className="list-group-flush">
-					<ListGroupItem>Cras justo odio</ListGroupItem>
-					<ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-					<ListGroupItem>Vestibulum at eros</ListGroupItem>
-				</ListGroup>
-				<Card.Body>
-					<Card.Link href="#">Card Link</Card.Link>
-					<Card.Link href="#">Another Link</Card.Link>
-				</Card.Body>
-			</Card>
+			<div className="card-group">
+				{projects.map((element, i) => {
+					return (
+						<div key={i} className="col-sm-3 mt-5">
+							<div
+								className="card border-3 border-color #ff8c00 mb-3 "
+								style={{ maxWidth: "20rem", borderColor: "#ff8c00" }}
+							>
+								<div className="card-body bg-white">
+									<h5 className="card-title bg-white">{element.title}</h5>
+									<p className="card-text bg-white"> {element.position} </p>
+								</div>
+								<ul
+									className="list-group list-group-flush border-color #ff8c00"
+									style={{ backgroundColor: "#ff8c00" }}
+								>
+									<li className="list-group-item border-color #ff8c00 ">
+										{iterate(element.techStack)}
+									</li>
+									<li
+										className="list-group-item border-color"
+										style={{ border: "inherit" }}
+									>
+										{iterate(element.otherTech)}
+									</li>
+									<li className="list-group-item bg-white">
+										<ul class="list-inline bg-white">
+											{element.challenges.map((el, j) => {
+												return (
+													<li
+														key={j}
+														className="list-inline-item bg-white border-color"
+														style={{ border: "inherit" }}
+													>
+														-{el}.
+													</li>
+												);
+											})}
+										</ul>
+									</li>
+								</ul>
+								<a href="#" className="btn btn-primary">
+									More Description
+								</a>
+								<div className="card-footer">
+									<small className="text-muted bg-white">
+										Last updated 3 mins ago
+									</small>
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
 		</div>
+	) : (
+		<h1> No projects</h1>
 	);
 }
 

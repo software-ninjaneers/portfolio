c6b6ec1292aae1037f7b9af3b55e4343490a5bcd
Seif-Miehiar <Seif.Miehiar@gmail.com>
2021-01-31 02:04:51 +0200

    deploying test

--- server.js
+++ server.js
@@ -29,6 +29,13 @@ app.get("/", (req, res) => {
 	res.send({ message: "Hello World!" });
 });
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "build", "index.html"));
+	});
+}
+
 // Launch app to listen to specified port
 app.listen(port, () => {
 	console.log(process.env.GMAIL);
--- src/component/projects.js
+++ src/component/projects.js
@@ -1,17 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Accordion, Card } from "react-bootstrap";
 import "../style/projects.css";
-import Accordion from "react-bootstrap/Accordion";
-// import Card from "react-bootstrap/Card";
-// import { ListGroup, ListGroupItem } from "react-bootstrap/";
 
 function Projects() {
 	const [projects, setProjects] = useState({ data: [] });
 
 	useEffect(async () => {
-		const result = await axios.get(
-			"http://localhost:3003/projectsApi/projects"
-		);
+		const result = await axios.get("/projectsApi/projects");
 
 		setProjects(result.data.data);
 	});
@@ -23,65 +19,81 @@ function Projects() {
 		return string;
 	};
 	return projects.length > 0 ? (
-		<div>
+		<div className="backgroundImg2">
 			<div className="card-group">
 				{projects.map((element, i) => {
 					return (
 						<div key={i} className="col-sm-3 mt-5">
-							<div
-								className="card border-3 border-color #ff8c00 mb-3 "
-								style={{
-									maxWidth: "20rem",
-									maxHeight: "100%",
-									borderColor: "#ff8c00",
-								}}
-							>
-								<div className="card-body bg-white">
-									<h5 className="card-title bg-white">{element.title}</h5>
-									<p className="card-text bg-white text-right text-muted">
-										{" "}
-										{element.position}{" "}
-									</p>
+							<Accordion defaultActiveKey="0">
+								<div
+									className="card border-3 border-color #ff8c00 mb-3 "
+									style={{
+										maxWidth: "20rem",
+										maxHeight: "100%",
+										borderColor: "#ff8c00",
+									}}
+								>
+									<div className="card-body">
+										<h5 className="card-title text-center font-weight-bold text-monospace">
+											{element.title}
+										</h5>
+										<p className="card-text  text-right text-muted">
+											{" "}
+											{element.position}
+										</p>
+									</div>
+									<ul className="list-group list-group-flush border-color #ff8c00 ">
+										<li className="list-group-item border-color #ff8c00 text-center ">
+											{element.desc}
+										</li>
+
+										<li
+											className="list-group-item border-color #ff8c00 text-center "
+											style={{ border: "inherit" }}
+										>
+											<Accordion.Toggle as={Card.Header} eventKey="0">
+												Technologies
+											</Accordion.Toggle>
+											<Accordion.Collapse eventKey="0">
+												<Card.Body> {iterate(element.techStack)}</Card.Body>
+											</Accordion.Collapse>
+										</li>
+										<li
+											className="list-group-item border-color text-center"
+											style={{ border: "inherit" }}
+										>
+											<Accordion.Toggle as={Card.Header} eventKey="1">
+												Management
+											</Accordion.Toggle>
+											<Accordion.Collapse eventKey="1">
+												<Card.Body>{iterate(element.otherTech)}</Card.Body>
+											</Accordion.Collapse>
+										</li>
+										<li className="list-group-item text-center">
+											<ul className="list-inline ">
+												<Accordion.Toggle as={Card.Header} eventKey="2">
+													Challenges
+												</Accordion.Toggle>
+												<Accordion.Collapse eventKey="2">
+													<Card.Body>
+														{element.challenges.map((el, j) => {
+															return (
+																<li
+																	key={j}
+																	className="list-inline-item  border-color text-left"
+																	style={{ border: "inherit" }}
+																>
+																	-{el}.
+																</li>
+															);
+														})}
+													</Card.Body>
+												</Accordion.Collapse>
+											</ul>
+										</li>
+									</ul>
 								</div>
-								<ul className="list-group list-group-flush border-color #ff8c00">
-									<li className="list-group-item border-color #ff8c00 ">
-										{element.desc}
-									</li>
-									<li
-										className="list-group-item border-color #ff8c00 "
-										style={{ border: "inherit" }}
-									>
-										{iterate(element.techStack)}
-									</li>
-									<li
-										className="list-group-item border-color"
-										style={{ border: "inherit" }}
-									>
-										{iterate(element.otherTech)}
-									</li>
-									<li className="list-group-item bg-white">
-										<ul className="list-inline bg-white">
-											{element.challenges.map((el, j) => {
-												return (
-													<li
-														key={j}
-														className="list-inline-item bg-white border-color"
-														style={{ border: "inherit" }}
-													>
-														-{el}.
-													</li>
-												);
-											})}
-										</ul>
-									</li>
-								</ul>
-								{/* <ProjectDesc key={i} data={element} /> */}
-								<div className="card-footer">
-									<small className="text-muted bg-white">
-										Last updated 3 mins ago
-									</small>
-								</div>
-							</div>
+							</Accordion>
 						</div>
 					);
 				})}
